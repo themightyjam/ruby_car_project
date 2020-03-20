694d2340ab7e77b32cefe39c550d359d485bda9e
Shaun Jerdan <s.jerdan93@gmail.com>
2020-03-20 11:59:18 +0000

    create table manufacturers

--- db/car_shop.sql
+++ db/car_shop.sql
@@ -1,4 +1,5 @@
 DROP TABLE cars;
+DROP TABLE manufacturers;
 
 CREATE TABLE cars (
   id SERIAL PRIMARY KEY,
@@ -7,5 +8,15 @@ CREATE TABLE cars (
   buying_cost INT,
   selling_price INT,
   stock INT,
-  manufacturer_id INT REFERENCES,
+  manufacturer_id INT REFERENCES
 );
+
+CREATE TABLE manufacturers (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR (255),
+  year_formed INT
+);
+
+
+
+)
