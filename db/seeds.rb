bacf3c76111451e443c625772f9262147c9610de
Shaun Jerdan <s.jerdan93@gmail.com>
2020-03-20 12:17:19 +0000

    initial seeds car commitgst

---
+++ db/seeds.rb
@@ -0,0 +1,20 @@
+require_relative('../models/car')
+require_relative('../models/manufacturer')
+
+car1 = Car.new({
+  "name" => "mp4/13",
+  "year_made" => 1998,
+  "buying_cost" => 100000,
+  "selling_price" => 300000,
+  "stock" => 2,
+  "manufacturer_id" =>
+  })
+
+  car2 = Car.new({
+    "name" => "mp4/4",
+    "year_made" => 1988,
+    "buying_cost" => 5000000,
+    "selling_price" => 10000000,
+    "stock" => 1,
+    "manufacturer_id" =>
+    })
