a6382eeaf6a892fc5e95ccacf92fcc8d0fff885a
Shaun Jerdan <s.jerdan93@gmail.com>
2020-03-20 13:12:05 +0000

    car seeds complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -28,3 +28,36 @@ car1 = Car.new({
       "manufacturer_id" =>
 
       })
+
+      car4 = Car.new({
+        "name" => "126C",
+        "year_made" => 1981,
+        "buying_cost" => 2000000,
+        "selling_price" => 4000000,
+        "stock" => 3,
+        "manufacturer_id" =>
+        })
+
+        car5 = Car.new({
+          "name" => "W09",
+          "year_made" => 2018 ,
+          "buying_cost" => 1000000,
+          "selling_price" => 2000000,
+          "stock" => 4,
+          "manufacturer_id" =>
+          })
+
+          car6 = Car.new({
+            "name" => "W196",
+            "year_made" => 1955 ,
+            "buying_cost" => 8000000,
+            "selling_price" => 16000000,
+            "stock" => 1,
+            "manufacturer_id" =>
+            })
+car1.save()
+car2.save()
+car3.save()
+car4.save()
+car5.save()
+car6.save()
