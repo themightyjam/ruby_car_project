664242ddb214134ccf006f9ea61ea3568c009a5c
Shaun Jerdan <s.jerdan93@gmail.com>
2020-03-21 10:20:38 +0000

    car model initialize

--- models/car.rb
+++ models/car.rb
@@ -3,3 +3,13 @@ require_relative('../db/sql_runner')
 class Car
 
   attr_reader :name, :year_made, :buying_cost, :selling_price, :stock
+  attr_accessor :manufacturer_id
+
+  def initialize(options)
+    @id = options['id'].to_i
+    @name = options['name']
+    @year_made = options['buying_cost']
+    @selling_price = options['selling_price']
+    @stock = options['stock']
+    @manufacturer_id = options['manufacturer_id'].to_i
+  end
