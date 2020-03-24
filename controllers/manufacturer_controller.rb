9c4e15f88951b4ff478ba5f0a1ba168cee1e6e96
Shaun Jerdan <s.jerdan93@gmail.com>
2020-03-24 11:03:55 +0000

    ammended manufacturer index

--- controllers/manufacturer_controller.rb
+++ controllers/manufacturer_controller.rb
@@ -4,6 +4,7 @@ require_relative('../models/car.rb')
 require_relative('../models/manufacturer.rb')
 also_reload('./models/*')
 
+
 get '/manufacturer' do
   @manufacturers = Manufacturer.all()
 erb(:"manufacturer/index")
