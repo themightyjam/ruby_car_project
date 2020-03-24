aa5075817b777d71f7077f09edaee98bc8d855ab
Shaun Jerdan <s.jerdan93@gmail.com>
2020-03-24 10:59:08 +0000

    manufacturer index.erb

--- controllers/manufacturer_controller.rb
+++ controllers/manufacturer_controller.rb
@@ -3,3 +3,8 @@ require('sinatra/contrib/all')
 require_relative('../models/car.rb')
 require_relative('../models/manufacturer.rb')
 also_reload('./models/*')
+
+get '/manufacturer' do
+  @manufacturers = Manufacturer.all()
+erb(:"manufacturer/index")
+end
